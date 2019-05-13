ecb541b01af9835da5948a4e25c5a2fae2febc44
Joanne Huang <prince811009@gmail.com>
2019-05-13 22:56:54 +0800

    hw3

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,14 @@
 function alphaSwap(str) {
-  console.log(str);
+  let answer = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      answer += str[i].toLowerCase();
+    } else if (str[i] >= 'a' && str[i] <= 'z') {
+      answer += str[i].toUpperCase();
+    } else answer += str[i];
+  }
+  return answer;
 }
 
 module.exports = alphaSwap;
+console.log(alphaSwap('A1Lkk2'));
