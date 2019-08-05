518262baac5d9984df9113f3ae37ccdf5ee20216
joanne <prince811009fy@gmail.com>
2019-08-05 16:20:20 +0800

    week9

--- homeworks/week9/hw2/utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function printMessage($msg, $redirect) {
-		echo '<script>';
-		echo "alert('" . htmlentities($msg, ENT_QUOTES) . "');";
-		echo "window.location = '" . $redirect . "'";
-		echo '</script>';
-	}
-?>
