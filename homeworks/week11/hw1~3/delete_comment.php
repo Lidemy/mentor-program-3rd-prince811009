8f7fd37ff4647ff5963885faabb6c49d5fda710e
Joanne Huang <prince811009@gmail.com>
2019-08-09 17:35:00 +0800

    modify_week11

---
+++ homeworks/week11/hw1~3/delete_comment.php
@@ -0,0 +1,24 @@
+<?php
+	include_once('check_login.php');
+	require_once('conn.php');
+	require_once('utils.php');
+
+	if (
+		isset($_POST['id']) &&
+		!empty($_POST['id'])
+		) {
+			$id = $_POST['id'];
+			
+			$sql = "DELETE FROM prince811009_comments where id=$id or parent_id=$id";
+			if ($conn->query($sql)) {
+				//server redirect
+				header('Location: ./index.php');
+			} else {
+				// client redirect
+				printMessage($conn->error, './index.php');
+			}
+
+		} else {
+			printMessage('錯誤', './index.php');
+	}
+?>
